ac8ccec3b0d4052c82ff06b2c3eeac17168a194a
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2021-07-08 08:44:53 +0530

    Initial Commit

---
+++ src/main/java/com/tushar/githooks/GithooksApplication.java
@@ -0,0 +1,13 @@
+package com.tushar.githooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tushar/githooks/GithooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.githooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
